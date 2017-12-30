786650080446695041a6dee5006db465b20a674a
Paige Henderson <pagingpaige943@gmail.com>
2017-12-30 14:54:41 -0800

    the header now redirects to dashbaord when user is logged in

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,1 +1,14 @@
-//this is a file
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: 'center'}}>
+			<h1>
+				Emaily!
+			</h1>
+			Collect feedback from your users
+		</div>
+	)
+};
+
+export default Landing;
