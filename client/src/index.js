48423fa6b6a7ddff48c71371a297a99fb1c9805b
Paige Henderson <pagingpaige943@gmail.com>
2017-12-30 14:16:32 -0800

    added header, configured redux, connected header functionality with backend

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 //redux stuffs, data layer control
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 
 import App from './components/App';
+import reducers from './reducers';
+
+//creating an action creator
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+	<Provider store={store}><App /></Provider>,
+	 document.querySelector('#root')
+);
 
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,12 +9,16 @@ app.get(
 //the things that could be wrong is where passport tries access the callback
 //this could be wrong
 app.get(
-	 '/auth/google/callback',
-		passport.authenticate('google'));
+	'/auth/google/callback',
+	passport.authenticate('google'),
+	(req, res) => {
+		res.redirect('/surveys');
+	}
+);
 
-app.get('/api/logout', (req, res) => {
+app.get('/api/logout', (req, res) => {	
 	req.logout();
-	res.send(req.user);
+	res.redirect('/');
 });
 
 app.get('/api/current_user', (req, res) => {
