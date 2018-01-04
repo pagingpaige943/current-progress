7f8b16d4719e451f33a46b7691cd84c452243d1f
Paige Henderson <pagingpaige943@gmail.com>
2018-01-03 21:31:44 -0800

    added some of the config for this app to be production ready

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ app.use(
 		keys: [keys.cookieKey]
 	})
 );
-//supposably magic commands that make everything work
+
+// initialize passport oauth middleware
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -28,6 +29,19 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets,
+	// like our main.js file, or main.css file
+	app.use(express.static('client/build'));
+
+	// express will serve up index.html file
+	// if it doesnt recognize route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
